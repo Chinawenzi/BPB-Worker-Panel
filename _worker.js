a9f3a7aac54e7b7bd3c18ef528e7935a38936423
一只大蚊子 <95013919+Chinawenzi@users.noreply.github.com>
2024-07-16 02:02:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'be3da0d2-0409-4480-88ed-3f31127e4e1c';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
